a1c6456b975c4e3ac04fd01cc34fb8f6f5c24186
bakriansyori <bakriansyori@gmail.com>
2026-02-09 17:10:41 +0700

    Update App.tsx
    Signed-off-by: bakriansyori <bakriansyori@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/kamika-sweet-landing">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
