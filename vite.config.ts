6ae2a0bb55ce1bd4036a63ea02a0b6bb65986289
bakriansyori <bakriansyori@gmail.com>
2026-02-09 15:37:36 +0700

    Update vite.config.ts
    Signed-off-by: bakriansyori <bakriansyori@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+base: '/kamika-sweet-landing/' // Tambahan baru untuk base direktori  
   server: {
     host: "::",
     port: 8080,
